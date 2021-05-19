9de887e5ebef0651d10f4f23080f02846b36c33f
unsiga25 <boloks25@gmail.com>
2021-05-20 00:41:08 +0800

    may 19 5pm

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,16 @@ window.onscroll = () => {
   else navbar.className = "navbar--scroll";
 };
 
+var elem = document.querySelector(".carousel");
+var flkty = new Flickity(elem, {
+  // options
+  cellAlign: "center",
+  contain: true,
+  adaptiveHeight: true,
+});
+
+// element argument can be a selector string
+//   for an individual element
 var flkty = new Flickity(".carousel", {
-  freeScroll: true,
-  wrapAround: true,
+  // options
 });
